1c73c4af0811541dc758764254924633cb552608
Christian Brennecke-Räther <c.b.raether@gmail.com>
2026-01-01 22:21:32 +0100

    guard stats functions against empty dataframes

--- bikeshare.py
+++ bikeshare.py
@@ -78,7 +78,6 @@ def load_data(city, month, day):
 
     return df
 
-
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
@@ -181,12 +180,16 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
+        if df.empty:
+            print("No data available for the selected filters.")
 
-        show_raw_data(df)
+        else:
+            time_stats(df)
+            station_stats(df)
+            trip_duration_stats(df)
+            user_stats(df)
+
+            show_raw_data(df)
   
         restart = get_input('\nWould you like to restart?', ['yes', 'no'], default='no')
         if restart!= 'yes':
